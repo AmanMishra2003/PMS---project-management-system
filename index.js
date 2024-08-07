02a239ed5cd6026f5154d81720a0ef78b9a43e58
Aman Mishra <mishraaman2021@gmail.com>
2024-08-07 13:15:46 +0530

    deploy

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ mongoose.connect(DbUrl).then(()=>{
 //required Middelware
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views')); 
+// app.set('views', path.join(__dirname, 'views')); 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
